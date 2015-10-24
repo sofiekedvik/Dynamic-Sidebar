7c9dd98f88ed6e1ad3d196534ccded8a3df772af
Sofie kedvik <sofie.kedvik@gmail.com>
2015-10-24 17:08:08 +0200

    first commit but defenitly not last

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+
+resizer();
+
+window.onresize = resizer;
+
+function resizer () {	
+	
+	if (window.innerWidth > 1024) {
+		var newHeight;
+		var main = document.getElementById('main');
+		newHeight = main.offsetHeight;	
+		document.getElementById('header').setAttribute('style', 'height:' + newHeight + "px");
+		
+	} else{
+		var main = document.getElementById('main');
+		document.getElementById('header').setAttribute('style', 'height:' + "auto");
+	}
+
+}	
+
+	console.log(newHeight);
+	console.log(main);
+
+
+
