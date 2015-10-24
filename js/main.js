4bf0f7f42e4d09e6177071a21319d11dd8c958a6
Sofie kedvik <sofie.kedvik@gmail.com>
2015-10-24 22:31:16 +0200

    explaining the story, added some icon

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
+var newHeight, mainNavItem, NavItem;
+mainNavItem = document.getElementsByClassName("wiki");
+NavItem = document.getElementsByClassName("hide");
 
+// for(var i = 0; i < mainNavItem.length; i++){
+// 	mainNavItem[i].addEventListener("click", function(){
+// 		for(var j = 0; j < NavItem.length; j++){
+// 			NavItem[j].classList.toggle("hide");
+// 		}
+// 	});
+// }
 
 window.onresize = resizer;
 
 function resizer () {	
 	
 	if (window.innerWidth > 1024) {
-		var newHeight;
 		var main = document.getElementById('main');
 		newHeight = main.offsetHeight;	
 		document.getElementById('header').setAttribute('style', 'height:' + newHeight + "px");
@@ -17,8 +26,8 @@ function resizer () {
 
 }	
 
-	console.log(newHeight);
-	console.log(main);
+console.log(newHeight);
+console.log(main);
 
 
 
